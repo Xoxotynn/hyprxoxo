bbaa9a4ce0e54661a7b604dc819e839a99e59f8c
Edward Loginov <tamerlan701@gmail.com>
2024-07-30 16:51:39 +0700

    Fixed git cloning in wine installation

--- scripts/install_wine.sh
+++ scripts/install_wine.sh
@@ -20,6 +20,7 @@ if ! pkg_installed wine-tkg-staging-git; then
   # install wine-staging with all optional dependencies
   $aurhlpr -S wine-staging winetricks wine-mono giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader
 
+  rm -rf $GitPkgDir/wine-tkg-git
   git clone https://github.com/Frogging-Family/wine-tkg-git.git $GitPkgDir/wine-tkg-git/
   sed -i -e 's/^_use_esync=\".*\"$/_use_esync="false"/
              s/^_use_fsync=\".*\"$/_use_fsync="true"/
@@ -47,6 +48,7 @@ WINEPREFIX=$HOME/.wine32 WINEARCH=win32
 
 # build dxvk with customized configs if needed
 if ! pkg_installed dxvk-mingw; then
+  rm -rf $GitPkgDir/PKGBUILDs
   git clone https://github.com/loathingKernel/PKGBUILDs $GitPkgDir/PKGBUILDs/
   sed -i -e 's/patch -p1 -i "$srcdir"\/1582\.patch//g
              s/patch -p1 -i "$srcdir"\/1582-fix-include\.patch//g
