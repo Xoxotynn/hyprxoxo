4ecbd7a3921303580434c0604f611ff849929ed6
Xoxotynn <tamerlan701@gmail.com>
2024-09-21 04:19:05 +0700

    Fixed gpu clocking

--- scripts/install_services.sh
+++ scripts/install_services.sh
@@ -12,3 +12,9 @@ if [ ! -f /etc/udev/rules.d/81-bluetooth-hci.rules ]; then
 else
   cat $CloneDir/rules/81-bluetooth-hci.rules | sudo tee -a /etc/udev/rules.d/81-bluetooth-hci.rules
 fi
+
+if [ ! -f /etc/udev/rules.d/30-amdgpu-high-power.rules ]; then
+  sudo cp $CloneDir/rules/30-amdgpu-high-power.rules /etc/udev/rules.d/
+else
+  cat $CloneDir/rules/30-amdgpu-high-power.rules | sudo tee -a /etc/udev/rules.d/30-amdgpu-high-power.rules
+fi
